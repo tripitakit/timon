3c29dd53ae1f7999ce7d7ba22ff06df36e31eaf5
Patrick De Marta <patrick.demarta@gmail.com>
2013-11-07 01:38:16 +0100

    rename adapter

--- app/models/timons.js
+++ app/models/timons.js
@@ -2,7 +2,7 @@ exports.definition = {
 
 	config: {
 	    	adapter: {
-			type: "mongodb",
+			type: "ti-mongodb",
 			collection_name: "timons",
 			base_url: "http://192.168.1.100:3000/timons/"
 		}
